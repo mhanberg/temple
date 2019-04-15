3e3d17c1b91771fab002e6d8bc7b14e6eca5d053
Mitchell Hanberg <mitch@mitchellhanberg.com>
2019-04-15 18:36:36 -0400

    add html nonvoid element

--- lib/dsl/html.ex
+++ lib/dsl/html.ex
@@ -2,6 +2,7 @@ defmodule Dsl.Html do
   alias Phoenix.HTML
 
   @nonvoid_elements ~w[
+    html
     head title style script
     noscript template
     body section nav article aside h1 h2 h3 h4 h5 h6
