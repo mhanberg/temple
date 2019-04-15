c56c21880db40b721b4e343d2fc314ff923e0092
Mitchell Hanberg <mitch@mitchellhanberg.com>
2019-04-14 22:35:13 -0400

    Reset formatter config

--- .formatter.exs
+++ .formatter.exs
@@ -1,12 +1,4 @@
 # Used by "mix format"
 [
-  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
-  export: [
-    locals_without_parens: [
-      htm: 1,
-      partial: :*,
-      flex: :*
-      div: :*
-    ]
-  ]
+  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
 ]
