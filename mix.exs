f40a4eae10510c95cc83c76b96bb5d9690ba7291
Michał Łępicki <michallepicki@users.noreply.github.com>
2020-03-19 00:41:37 +0100

    try removing plug from deps completely

--- mix.exs
+++ mix.exs
@@ -59,7 +59,6 @@ defmodule Temple.MixProject do
       {:ex_doc, "~> 0.0", only: [:dev], runtime: false},
       {:html_sanitize_ex, "~> 1.3", only: [:dev, :test], runtime: false},
       {:phoenix, "~> 1.4", optional: true},
-      {:plug, "~> 1.7", optional: true},
       {:floki, "~> 0.23.0", only: [:dev, :test], runtime: false}
     ]
   end
