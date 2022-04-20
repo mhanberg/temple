79fb98a722f77783f32c7120fc4f88ea1034e81c
Mitchell Hanberg <mitch@mitchellhanberg.com>
2022-04-20 00:00:11 -0400

    Remove phx generator templates

--- priv/templates/temple.gen.html/edit.html.exs
+++
@@ -1,7 +0,0 @@
-h1 do: "Edit <%= schema.human_singular %>"
-
-render("form.html", Map.put(assigns, :action, Routes.<%= schema.route_helper %>_path(@conn, :update, @<%= schema.singular %>)))
-
-span do
-  link "Back", to: Routes.<%= schema.route_helper %>_path(@conn, :index)
-end
--- priv/templates/temple.gen.html/new.html.exs
+++
@@ -1,7 +0,0 @@
-h1 do: "New <%= schema.human_singular %>"
-
-render("form.html", Map.put(assigns, :action, Routes.<%= schema.route_helper %>_path(@conn, :create)))
-
-span do
-  link "Back", to: Routes.<%= schema.route_helper %>_path(@conn, :index)
-end
