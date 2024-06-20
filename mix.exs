f28468e7f877a39759696333c4135fb1aa877d11
Mitchell Hanberg <mitch@mitchellhanberg.com>
2024-06-20 08:33:50 -0400

    fix: relax constraint on phoenix_html

--- mix.exs
+++ mix.exs
@@ -73,7 +73,7 @@ defmodule Temple.MixProject do
   defp deps do
     [
       {:floki, ">= 0.0.0"},
-      {:phoenix_html, "~> 3.2"},
+      {:phoenix_html, "~> 3.2 or ~> 4.0"},
       {:typed_struct, "~> 0.3"},
       {:ex_doc, "~> 0.34.0", only: :dev, runtime: false}
     ]
