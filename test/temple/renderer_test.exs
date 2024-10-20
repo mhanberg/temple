3915a656248682905d9a1b889aae7a3a65f61288
Mitchell Hanberg <mitch@mitchellhanberg.com>
2024-10-20 19:15:39 -0400

    fix: allow normal list elements in class attr
    This allows you to include strings in class lists instead of only
    allowing keyword lists.
    
    So `["foo", "bar", nil, "baz": true]` will result in `"foo bar baz"` now
    
    Fixes #238

--- test/temple/renderer_test.exs
+++ test/temple/renderer_test.exs
@@ -485,18 +485,26 @@ defmodule Temple.RendererTest do
   end
 
   describe "special attribute stuff" do
-    test "class object syntax" do
+    test "class list " do
       result =
         Renderer.compile do
-          div class: ["hello world": false, "text-red": true] do
+          div class: ["foo bar", "hello world": false, "text-red": true] do
             "hello world"
+          end
+
+          div class: ["foo", nil, "bar"] do
+            "hi"
           end
         end
 
       # html
       expected = """
-      <div class="text-red">
+      <div class="foo bar text-red">
         hello world
+      </div>
+
+      <div class="foo bar">
+        hi
       </div>
 
       """
