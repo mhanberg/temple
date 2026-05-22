1becda2423637a86ce28ebd5390b2114bc3e64f0
Sai Asish Y <say.apm35@gmail.com>
2026-05-22 01:24:50 -0700

    test(temple): assert SVG and MathML aliases produce expected tags

--- test/temple_test.exs
+++ test/temple_test.exs
@@ -29,6 +29,54 @@ defmodule TempleTest do
 
       assert expected == result
     end
+
+    test "compiles an SVG element alias" do
+      assigns = %{}
+      _ = assigns
+
+      result =
+        temple do
+          text__ x: "10", y: "20" do
+            "label"
+          end
+        end
+        |> Phoenix.HTML.safe_to_string()
+
+      assert result =~ ~s|<text x="10" y="20">|
+      assert result =~ "label"
+      assert result =~ "</text>"
+    end
+
+    test "compiles an SVG void element alias" do
+      assigns = %{}
+      _ = assigns
+
+      result =
+        temple do
+          path__ d: "M0 0"
+        end
+        |> Phoenix.HTML.safe_to_string()
+
+      assert result =~ ~s|<path d="M0 0"|
+      refute result =~ "</path>"
+    end
+
+    test "compiles a MathML element alias" do
+      assigns = %{}
+      _ = assigns
+
+      result =
+        temple do
+          mtext__ do
+            "label"
+          end
+        end
+        |> Phoenix.HTML.safe_to_string()
+
+      assert result =~ "<mtext>"
+      assert result =~ "label"
+      assert result =~ "</mtext>"
+    end
   end
 
   describe "attributes/1" do
